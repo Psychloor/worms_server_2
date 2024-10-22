20d912f869385418073370aeeb49d22133d8d6bd
Psychloor <blomqvistkevin@hotmail.com>
2024-10-22 18:40:52 +0200

    Refactor database hashing and add ID recycling
    Update DashMap structures to use specialized hashers for efficiency. Introduce a mechanism to recycle IDs using a mutex-protected list to prevent ID exhaustion and improve performance.

--- src/database.rs
+++ src/database.rs
@@ -6,15 +6,19 @@ use crate::database::game::Game;
 use crate::database::room::Room;
 use crate::database::user::User;
 use dashmap::DashMap;
+use nohash_hasher::BuildNoHashHasher;
+use rustc_hash::FxBuildHasher;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub struct Database {
-    pub users: DashMap<u32, User>,
-    pub rooms: DashMap<u32, Room>,
-    pub games: DashMap<u32, Game>,
-    pub user_to_game: DashMap<String, u32>,
+    pub users: DashMap<u32, User, BuildNoHashHasher<u32>>,
+    pub rooms: DashMap<u32, Room, BuildNoHashHasher<u32>>,
+    pub games: DashMap<u32, Game, BuildNoHashHasher<u32>>,
+    pub user_to_game: DashMap<String, u32, FxBuildHasher>,
     next_id: AtomicU32,
+    reusable_ids: Arc<Mutex<Vec<u32>>>,
 }
 
 impl Database {
@@ -23,16 +27,41 @@ impl Database {
 
     pub fn new() -> Arc<Self> {
         Arc::new(Self {
-            users: DashMap::with_capacity(Database::STARTING_CAPACITY),
-            rooms: DashMap::with_capacity(Database::STARTING_CAPACITY),
-            games: DashMap::with_capacity(Database::STARTING_CAPACITY),
-            user_to_game: DashMap::with_capacity(Database::STARTING_CAPACITY),
+            users: DashMap::with_capacity_and_hasher(
+                Self::STARTING_CAPACITY,
+                BuildNoHashHasher::default(),
+            ),
+            rooms: DashMap::with_capacity_and_hasher(
+                Self::STARTING_CAPACITY,
+                BuildNoHashHasher::default(),
+            ),
+            games: DashMap::with_capacity_and_hasher(
+                Self::STARTING_CAPACITY,
+                BuildNoHashHasher::default(),
+            ),
+            user_to_game: DashMap::with_capacity_and_hasher(
+                Database::STARTING_CAPACITY,
+                FxBuildHasher,
+            ),
             next_id: AtomicU32::new(Database::ID_START),
+            reusable_ids: Arc::new(Mutex::new(Vec::new())),
         })
     }
 
     pub async fn get_next_id(db: &Arc<Database>) -> u32 {
+        let mut lock = db.reusable_ids.lock().await;
+        if let Some(id) = lock.pop() {
+            return id;
+        }
+
         db.next_id.fetch_add(1, Ordering::Relaxed)
+    }
+
+    pub async fn recycle_id(db: Arc<Database>, id: u32) {
+        if id >= Database::ID_START {
+            let mut lock = db.reusable_ids.lock().await;
+            lock.push(id);
+        }
     }
 
     pub async fn name_exists(db: &Arc<Database>, name: &str) -> bool {
