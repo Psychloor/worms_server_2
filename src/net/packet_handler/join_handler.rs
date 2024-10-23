a91f06f8f53e36d3463e023d8e27ad61510ca2c5
Psychloor <blomqvistkevin@hotmail.com>
2024-10-23 19:52:23 +0200

    Refactor indentation to use tabs instead of spaces
    Updated the indentation across multiple files to maintain consistency. This change affects encoding and decoding in worms_codec.rs, database structures, and several packet handlers. The logic remains unchanged.

--- src/net/packet_handler/join_handler.rs
+++ src/net/packet_handler/join_handler.rs
@@ -14,63 +14,63 @@ pub struct JoinHandler;
 
 #[async_trait]
 impl PacketHandler for JoinHandler {
-    async fn handle_packet(
-        tx: &Sender<Arc<Bytes>>,
-        packet: &Arc<WormsPacket>,
-        client_id: u32,
-        _address: &SocketAddr,
-    ) -> anyhow::Result<()> {
-        let db = &DATABASE;
+	async fn handle_packet(
+		tx: &Sender<Arc<Bytes>>,
+		packet: &Arc<WormsPacket>,
+		client_id: u32,
+		_address: &SocketAddr,
+	) -> anyhow::Result<()> {
+		let db = &DATABASE;
 
-        let join_id = packet.value_2.as_ref().map_or(0, |p| *p);
-        let user_room_id_original = db.users.get(&client_id).map_or(0, |u| u.room_id);
+		let join_id = packet.value_2.as_ref().map_or(0, |p| *p);
+		let user_room_id_original = db.users.get(&client_id).map_or(0, |u| u.room_id);
 
-        if join_id == 0 || packet.value_10 != Some(client_id) {
-            bail!("Invalid Data!");
-        }
+		if join_id == 0 || packet.value_10 != Some(client_id) {
+			bail!("Invalid Data!");
+		}
 
-        // Check rooms
-        if db.rooms.get(&join_id).is_some() {
-            db.users
-                .get_mut(&client_id)
-                .ok_or(anyhow!("User not found!"))?
-                .room_id = join_id;
+		// Check rooms
+		if db.rooms.get(&join_id).is_some() {
+			db.users
+				.get_mut(&client_id)
+				.ok_or(anyhow!("User not found!"))?
+				.room_id = join_id;
 
-            let packet = WormsPacket::create(PacketCode::Join)
-                .with_value_2(join_id)
-                .with_value_10(client_id)
-                .build()?;
-            Server::broadcast_all_except(packet, &client_id).await?;
+			let packet = WormsPacket::create(PacketCode::Join)
+				.with_value_2(join_id)
+				.with_value_10(client_id)
+				.build()?;
+			Server::broadcast_all_except(packet, &client_id).await?;
 
-            let packet = WormsPacket::create(PacketCode::JoinReply)
-                .with_error_code(0)
-                .build()?;
-            tx.send(packet).await?;
+			let packet = WormsPacket::create(PacketCode::JoinReply)
+				.with_error_code(0)
+				.build()?;
+			tx.send(packet).await?;
 
-            return Ok(());
-        } else if let Some(game) = db.games.get(&join_id) {
-            if game.room_id == user_room_id_original {
-                let packet = WormsPacket::create(PacketCode::Join)
-                    .with_value_2(join_id)
-                    .with_value_10(client_id)
-                    .build()?;
-                Server::broadcast_all_except(packet, &client_id).await?;
+			return Ok(());
+		} else if let Some(game) = db.games.get(&join_id) {
+			if game.room_id == user_room_id_original {
+				let packet = WormsPacket::create(PacketCode::Join)
+					.with_value_2(join_id)
+					.with_value_10(client_id)
+					.build()?;
+				Server::broadcast_all_except(packet, &client_id).await?;
 
-                let packet = WormsPacket::create(PacketCode::JoinReply)
-                    .with_error_code(0)
-                    .build()?;
-                tx.send(packet).await?;
+				let packet = WormsPacket::create(PacketCode::JoinReply)
+					.with_error_code(0)
+					.build()?;
+				tx.send(packet).await?;
 
-                return Ok(());
-            }
-        }
+				return Ok(());
+			}
+		}
 
-        // if we got to here then there was no room or game to join
-        let packet = WormsPacket::create(PacketCode::JoinReply)
-            .with_error_code(1)
-            .build()?;
-        tx.send(packet).await?;
+		// if we got to here then there was no room or game to join
+		let packet = WormsPacket::create(PacketCode::JoinReply)
+			.with_error_code(1)
+			.build()?;
+		tx.send(packet).await?;
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
