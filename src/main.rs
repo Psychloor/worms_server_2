e2e5caeafe1f2cf6694b4ba3592b58da8b7bf5a1
Psychloor <blomqvistkevin@hotmail.com>
2024-10-23 19:59:50 +0200

    Simplify main function and refactor packet handlers
    Refactored the main function by extracting environment initialization and Ctrl+C signal handling into separate functions, enhancing readability and maintainability. Simplified packet handler match arms in `packet_handler.rs` to improve code clarity.

--- src/main.rs
+++ src/main.rs
@@ -11,32 +11,32 @@ pub(crate) mod server;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
-	initialize_environment();
+    initialize_environment();
 
-	let args = Args::try_parse()?;
-	let cancellation_token = CancellationToken::new();
+    let args = Args::try_parse()?;
+    let cancellation_token = CancellationToken::new();
 
-	handle_ctrl_c_signal(cancellation_token.clone());
+    handle_ctrl_c_signal(cancellation_token.clone());
 
-	let server_address = SocketAddr::new(args.ip, args.port);
-	if let Err(e) = Server::start_server(server_address, cancellation_token).await {
-		log::error!("Server encountered an error: {}", e);
-	}
+    let server_address = SocketAddr::new(args.ip, args.port);
+    if let Err(e) = Server::start_server(server_address, cancellation_token).await {
+        log::error!("Server encountered an error: {}", e);
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn initialize_environment() {
-	dotenv::dotenv().ok();
-	pretty_env_logger::init();
+    dotenv::dotenv().ok();
+    pretty_env_logger::init();
 }
 
 fn handle_ctrl_c_signal(cancellation_token: CancellationToken) {
-	tokio::spawn(async move {
-		tokio::signal::ctrl_c()
-			.await
-			.expect("Failed to listen for Ctrl+C signal!");
-		log::info!("Server shutting down due to Ctrl+C");
-		cancellation_token.cancel();
-	});
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to listen for Ctrl+C signal!");
+        log::info!("Server shutting down due to Ctrl+C");
+        cancellation_token.cancel();
+    });
 }
